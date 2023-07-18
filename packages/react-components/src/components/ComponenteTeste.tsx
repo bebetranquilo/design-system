7fff4234e368048bdd4644789efdf241bc856d46
Luigi Oliveira <luigi_oliveira_ext@carrefour.com>
2023-07-18 19:28:00 -0300

    First Commit Test

---
+++ packages/react-components/src/components/ComponenteTeste.tsx
@@ -0,0 +1,5 @@
+import style from "@bb-design-system/styles/src/ComponenteTeste.module.scss";
+
+export const ComponenteTeste = () => {
+  return <button className={style.botaoLegal}>BELEZA</button>;
+};
