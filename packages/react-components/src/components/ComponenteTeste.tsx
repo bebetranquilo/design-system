9da6c529d4b3aaff07fb2c44955657a2ef433ef3
Luigi Oliveira <luigi_oliveira_ext@carrefour.com>
2023-07-18 19:38:58 -0300

    test husky

--- packages/react-components/src/components/ComponenteTeste.tsx
+++ packages/react-components/src/components/ComponenteTeste.tsx
@@ -1,5 +1,9 @@
 import style from "@bb-design-system/styles/src/ComponenteTeste.module.scss";
 
 export const ComponenteTeste = () => {
-  return <button className={style.botaoLegal}>BELEZA</button>;
+  return (
+    <>
+      <button className={style.botaoLegal}>BELEZA</button>;
+    </>
+  );
 };
