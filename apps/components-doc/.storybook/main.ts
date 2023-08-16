974a9835249869cd554ba187e3c57559721a0a38
Luigi Oliveira <luigi_oliveira_ext@carrefour.com>
2023-08-15 21:05:29 -0300

    Feat: Deploy changes

--- apps/components-doc/.storybook/main.ts
+++ apps/components-doc/.storybook/main.ts
@@ -24,5 +24,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/design-system/";
+    }
+
+    return config;
+  },
 };
 export default config;
